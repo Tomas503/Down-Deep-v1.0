37cfcc663189d196f6aafcbe395435e30953571a
Tomas503 <tomas503@gmail.com>
2015-06-19 21:28:54 -0700

    Door open atempt
    Fail, back to before.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+			
+			//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//RaycastHit rayCastHit;
+
+			Debug.Log("Mouse Click");
+			
+			//if(Physics.Raycast(ray.origin, ray.direction, out rayCastHit, Mathf.Infinity)){
+				
+
+			//	Debug.Log("Mouse Click on door");
+			//}
+			
+		} 
+			
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -28,6 +28,21 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rayCastHit;
+
+			if(Physics.Raycast(ray.origin, ray.direction, out rayCastHit, Mathf.Infinity)){
+
+				Debug.Log("Mouse Click");
+
+			}
+
+		}
+
+
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			RestartGame();
 				}
